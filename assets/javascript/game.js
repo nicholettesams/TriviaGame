3793769c3c0ad3f790bee0c3ad23d59c54aa1a36
nicholettesams <nicholettesams@msn.com>
2018-08-24 09:24:56 -0400

    Questions

---
+++ assets/javascript/game.js
@@ -0,0 +1,82 @@
+const myQuestions = [
+    {
+        question: "On the movie poster, a person is pictured covered in this office supply...",
+        answers: {
+            a: "Glue",
+            b: "Staples",
+            c: "Paper",
+            d: "Post-its"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "What brand is Milton's stapler?",
+        answers: {
+            a: "Staples",
+            b: "Stanley Bostitch",
+            c: "Swingline",
+            d: "Office Depot"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "How many pieced of flare did Joanna's co-worker, Brian,  wear?",
+        answers: {
+            a: "47",
+            b: "37",
+            c: "15",
+            d: "0"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What is the name of the character that shares their name with an American singer/songwriter?",
+        answers: {
+            a: "Bruce Springsteen",
+            b: "Peter Cetera",
+            c: "Bill Hudson",
+            d: "Michael Bolton"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "What piece of office equipment do Peter, Michael and Samir destroy?",
+        answers: {
+            a: "Printer",
+            b: "Fax Machine",
+            c: "Copier",
+            d: "Stapler"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What are the names of the two men that are in charge of evaluating the staff?",
+        answers: {
+            a: "Steve",
+            b: "Bob",
+            c: "Jim",
+            d: "Lawrence"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What is the name of the company that Peter works for?",
+        answers: {
+            a: "Initrode",
+            b: "Intel",
+            c: "Initech",
+            d: "Dunder Mifflin"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "How much 'actual work' did Peter say he does?",
+        answers: {
+            a: "15 minutes each day",
+            b: "15 minutes each week",
+            c: "15 minutes each month",
+            d: "none"
+        },
+        correctAnswer: "b"
+    }
+]
